c287efb1ca39abbf9d9a7ed5f132733b9f69cd94
João Pedro <joaobuenoh@hotmail.com>
2025-01-14 23:42:54 -0300

    feat: adiciona teste que valida anotacoes

--- back-end-maptriz/src/main/java/com/desafio/agenda/AgendaApplication.java
+++ back-end-maptriz/src/main/java/com/desafio/agenda/AgendaApplication.java
@@ -1,13 +1,24 @@
 package com.desafio.agenda;
 
+import com.desafio.agenda.model.PessoaFisica;
+import com.desafio.agenda.validators.AtLeastOne;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgendaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgendaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaApplication.class, args);
+
+        PessoaFisica pessoafisica = new PessoaFisica();
+
+
+        if (pessoafisica.getClass().isAnnotationPresent(AtLeastOne.class)) {
+            System.out.println("Works");
+        } else
+            System.out.println("Didn't work");
+    }
+
 
 }
